a0b91dd9d892fb47ec72879f0620d16a0519797b
dcuevas-dawi <cuevasnaharro@cifpfbmoll.eu>
2024-10-31 17:20:53 +0100

    second dani

--- scripts/database.js
+++ scripts/database.js
@@ -7,17 +7,17 @@ let concerts = new Array();
 // Funcion que cogera los inputs y los metera en nuestra lista de conciertos como un objeto nuevo
 function getNewConcert() {
     let concert = {
-        date: document.getElementById("").value, // fechaConcierto
-        daysBefore: document.getElementById("").value, // dias de antelacion
-        sellingDate: document.getElementById("").value, // fecha de venta
-        name: document.getElementById("").value, // nombre de concierto
+        date: document.getElementById("concertDate").value, // fechaConcierto
+        daysBefore: document.getElementById("daysBefore").value, // dias de antelacion
+        sellingDate: document.getElementById("saleDate").value, // fecha de venta
+        name: document.getElementById("concertName").value, // nombre de concierto
         validatedName: "", // nombre de concierto formateado - (Lo gestiona Oscar, no te rayes Dani) 
-        basePrice: document.getElementById("").value, // Precio base
-        discount: document.getElementById("").value, // Descuento
+        basePrice: document.getElementById("basePrice").value, // Precio base
+        discount: document.getElementById("discount").value, // Descuento
         ticketPrice: document.getElementById("").value, // Precio ticket
-        soldTickets: document.getElementById("").value, // Tickets vendidos
-        artistPercentage: document.getElementById("").value, // Lo que se lleva el artista
-        totalIncome: document.getElementById("").value, // Ingresos totales
+        soldTickets: document.getElementById("soldTickets").value, // Tickets vendidos
+        artistPercentage: document.getElementById("artistPercentage").value, // Lo que se lleva el artista
+        totalIncome: document.getElementById("totalIncome").value, // Ingresos totales
         maxTickets: document.getElementById("").value, // Tickets maximos
         assistans: document.getElementById("").value, // Asistentes
         purchasedTickets: document.getElementById("").value,
