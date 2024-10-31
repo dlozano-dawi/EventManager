baed894202b18d1992bba1a1ac823746483b0aed
oscar-laptop <oscarlmdev@gmail.com>
2024-10-31 17:17:54 +0100

    string functions

--- scripts/stringController.js
+++ scripts/stringController.js
@@ -1,1 +1,11 @@
-//David marikon
+function formatConcertName(name) {
+    return name.titleCase();
+}
+
+function createEventDescription(validatedName, artistName, date) {
+    return `Come see ${artistName} perform at ${validatedName} on ${date}.`;
+}
+
+function validateArtistName(artistName) {
+    return artistName.trim().titleCase();
+}
