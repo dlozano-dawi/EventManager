5e3d5e1e52d57f22bba43bf9f4dd5a9052db46b1
oscar-laptop <oscarlmdev@gmail.com>
2024-10-31 17:33:02 +0100

    string update plus test

--- scripts/stringController.js
+++ scripts/stringController.js
@@ -1,5 +1,9 @@
 function formatConcertName(name) {
-    return name.titleCase();
+    return name
+    .toLowerCase()
+    .split(" ") //Split the string into an array of words
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1)) //Capitalize the first letter of each word
+    .join(" "); //Join the array back into a string
 }
 
 function createEventDescription(validatedName, artistName, date) {
@@ -7,5 +11,7 @@ function createEventDescription(validatedName, artistName, date) {
 }
 
 function validateArtistName(artistName) {
-    return artistName.trim().titleCase();
+    return artistName.trim();
 }
+
+console.log(formatConcertName("the rolling stones"));
