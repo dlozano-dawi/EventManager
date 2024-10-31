455e4dd5bf7727854056b0bcf475ad226629ee84
dcuevas-dawi <cuevasnaharro@cifpfbmoll.eu>
2024-10-31 17:58:24 +0100

    fifth dani

--- scripts/database.js
+++ scripts/database.js
@@ -21,7 +21,8 @@ function getNewConcert() {
         maxTickets: document.getElementById("maxTickets").value, // Tickets maximos
         assistans: document.getElementById("assistans").value, // Asistentes
         requestedTickets: document.getElementById("requestedTickets").value, // requestedTickets
-        artistName: document.getElementById("artistName").value // Nombre artista
+        artistName: document.getElementById("artistName").value, // Nombre artista
+        id: generateConcertID() // ID para el concierto
     }
 
     concerts.push(concert)
