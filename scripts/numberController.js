46195b8f273b9ea8c862e3d8c85bc06d72a33b15
Marc <mcanadassuau@cifpfbmoll.eu>
2024-10-31 17:37:44 +0100

    mejorado

--- scripts/numberController.js
+++ scripts/numberController.js
@@ -1,4 +1,5 @@
-function validateAvailableTickets(requestedTickets, maxTickets) { //Verifica si el número de tickets solicitado no supera la capacidad máxima de la sala.
+function validateAvailableTickets(requestedTickets, maxTickets) {
+  //Verifica si el número de tickets solicitado no supera la capacidad máxima de la sala.
   if (requestedTickets > maxTickets) {
     return false;
   } else {
@@ -6,11 +7,13 @@ function validateAvailableTickets(requestedTickets, maxTickets) { //Verifica si 
   }
 }
 
-function calculateIncomePerAssistant(totalIncome, assistants) { //Calcula el ingreso promedio por asistente, dado el ingreso total y el número de asistentes.
+function calculateIncomePerAssistant(totalIncome, assistants) {
+  //Calcula el ingreso promedio por asistente, dado el ingreso total y el número de asistentes.
 
   return totalIncome / assistants;
 }
 
-function generateConcertID(concertDate, basePrize) { //Genera un ID único para el concierto combinando la fecha y el precio del ticket.
+function generateConcertID(concertDate, basePrize) {
+  //Genera un ID único para el concierto combinando la fecha y el precio del ticket.
   return concertDate + basePrize;
 }
