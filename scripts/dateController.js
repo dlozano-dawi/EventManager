4563dd9e7d975375f8fb3d4c80bc18019e41b819
examengit <gitexamen@gmail.com>
2024-11-07 16:50:11 +0100

    Added: getSeason and calculateAntelation functions

--- scripts/dateController.js
+++ scripts/dateController.js
@@ -1,5 +1,5 @@
 function makeReminder(date, days) {
-    let formatedDate = date.toString().split("/").reverse().join("/");
+    let formatedDate = date.toString().split("-").reverse().join("-");
     
     return subtractTimeFromDate(Date.parse(formatedDate), days);
 }
@@ -8,4 +8,14 @@ function subtractTimeFromDate(date, days) {
     var addMlSeconds = ((days * 24) * 60) * 60000;
     var newDateObj = new Date(date - addMlSeconds);
     return newDateObj;
+}
+
+function getSeason(date) {
+    // TODO 
+    return "not implemented yet";
+}
+
+function calculateAntelation(concertDate, sellingDate) {
+    console.log(Date.parse(concertDate))
+    console.log(Date.parse(sellingDate))
 }
