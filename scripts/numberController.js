bdfd56afa6fc800186ce39368e26577f68c2ef64
examengit <gitexamen@gmail.com>
2024-11-13 16:07:13 +0100

    push

--- scripts/numberController.js
+++ scripts/numberController.js
@@ -24,7 +24,37 @@ function buyTickets(id) {
   let tickets = document.getElementById("ticketAmount").value
   let discount = document.getElementById("discountCode").value
   concerts[id].soldTickets = tickets
+  total = getPrice(id, tickets, discount)
+  concerts[id].totalIncome = total
+  
+  let output = "La cantidad a pagar es: " + total
+
   showConcert()
   modifyConcert()
+
+  document.getElementById("showConcerts").innerHTML += 
+    "<br><br>" +
+    output
+}
+
+function getPrice(id, tickets, discount) {
+  let discountAplicated = 1;
+  switch (discount) {
+    case "SUMMER20":
+      discountAplicated = 0.8;
+      break;
+    case "WINTER20":
+      discountAplicated = 0.8;
+      break;
+    case "SPRING15":
+      discountAplicated = 0.85;
+      break;
+    case "AUTUMN10":
+      discountAplicated = 0.9;
+      break;
+    default:
+      break;
+  }
+  return +concerts[id].basePrice * +tickets * +discountAplicated;
 }
 
