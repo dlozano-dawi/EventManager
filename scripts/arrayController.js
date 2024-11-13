d712fc4eb81817389716e9db4df2e8cbaf6424e3
examengit <gitexamen@gmail.com>
2024-11-13 16:32:14 +0100

    a

--- scripts/arrayController.js
+++ scripts/arrayController.js
@@ -51,8 +51,8 @@ function showConcert(){
             "Tickets restantes: " + (concerts[i].maxTickets - concerts[i].soldTickets) + "<br>" +
             "Asistentes Maximos: " + concerts[i].maxTickets + "<br>" +
             "Nombre artista: " + concerts[i].artistName + "<br>" +
-            "Ganancias artista: " + Number(concerts[i].totalIncome) * Number(concerts[i].artistPercentage) + "<br>" +
-            "Ganancias organización: " + Number(concerts[i].totalIncome) - (Number(concerts[i].totalIncome) * Number(concerts[i].artistPercentage)) + "<br>" +
+            "Ganancias artista: " + (Number(concerts[i].totalIncome) * Number(concerts[i].artistPercentage)) + "<br>" +
+            "Ganancias organización: " + ((Number(concerts[i].totalIncome) - Number(concerts[i].totalIncome)) * Number(concerts[i].artistPercentage)) + "<br>" +
             "ID: " + concerts[i].id + "</div>" +
             "Estacion: " + concerts[i].season + 
             "<br>";
